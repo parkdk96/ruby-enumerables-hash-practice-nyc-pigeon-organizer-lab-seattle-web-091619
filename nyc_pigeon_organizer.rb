ad18e046bd0db9197c54f07b250a06c2b05f8754
Daniel Park <parkdk96@gmail.com>
2019-08-29 08:37:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |key, val|
+    val.each do |prop, name_list|
+      name_list.each do |name|
+        pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+      end
+    end
+  end
+  
+  pigeon_list.length.times do |i|
+    data[:color].each do |color_key, name_list|
+      if name_list.include?(pigeon_list[i])
+        pigeon_list[i][:color].push(color_key.to_s)
+      end
+    end
+  end
+  
+  puts pigeon_list
 end
