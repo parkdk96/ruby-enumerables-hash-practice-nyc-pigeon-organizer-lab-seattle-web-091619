7d5ea9dd8414191864c03b35e3c252f9db8542e4
Daniel Park <parkdk96@gmail.com>
2019-08-29 08:38:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,39 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
-  pigeon_list = {}
+list_of_names = []
+pigeon_list = {}
   data.each do |key, val|
     val.each do |prop, name_list|
       name_list.each do |name|
         pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+        list_of_names.push(name)
       end
     end
   end
-  
-  pigeon_list.length.times do |i|
-    data[:color].each do |color_key, name_list|
-      if name_list.include?(pigeon_list[i])
-        pigeon_list[i][:color].push(color_key.to_s)
-      end
+
+list_of_names = list_of_names.uniq
+
+data[:color].each do |key, val|
+  val.length.times do |i| #this val is the array of names for each color
+    if list_of_names.include?(val[i])
+      pigeon_list[val[i]][:color].push(key.to_s)
     end
   end
-  
-  puts pigeon_list
+end
+
+data[:gender].each do |key, val|
+  val.length.times do |i|
+    if list_of_names.include?(val[i])
+    pigeon_list[val[i]][:gender].push(key.to_s)
+    end
+  end
+end
+
+data[:lives].each do |key, val|
+  val.length.times do |i|
+    if list_of_names.include?(val[i])
+    pigeon_list[val[i]][:lives].push(key.to_s)
+    end
+  end
+end
 end
