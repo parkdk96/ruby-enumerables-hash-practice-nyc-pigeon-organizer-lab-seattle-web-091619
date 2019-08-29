222df630e33ff7415ba9cc0f7ebcc942dca2b94f
Daniel Park <parkdk96@gmail.com>
2019-08-29 08:42:32 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,15 +7,15 @@ def nyc_pigeon_organizer(data)
         name_list.each do |name|
           pigeon_list[name] = {:color => [], :gender => [], :lives => []}
           list_of_names.push(name)
-        end
       end
     end
+  end
   
   # Making this list in order to avoid additional hash navigation while writing the conditional statements
   list_of_names = list_of_names.uniq
   
   data[:color].each do |key, val|
-    val.length.times do |i| #this val is the array of names for each color
+    val.length.times do |i| #val is an array of names for each color
       if list_of_names.include?(val[i])
         pigeon_list[val[i]][:color].push(key.to_s)
       end
